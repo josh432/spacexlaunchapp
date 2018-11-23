2018dd7f2612f76fc502bf20d2a363140d95f215
Josh Silverman <jrsilverman71@hotmail.com>
2018-11-23 14:24:32 -0800

    Finished React

---
+++ public/precache-manifest.4ba8dd73fbdb13561a62ea2a9a61144b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d577ade9a0ce72c4d4b",
+    "url": "/static/js/main.0d577ade.chunk.js"
+  },
+  {
+    "revision": "6f5940373e766b44f8bc",
+    "url": "/static/js/1.6f594037.chunk.js"
+  },
+  {
+    "revision": "0d577ade9a0ce72c4d4b",
+    "url": "/static/css/main.55b1177c.chunk.css"
+  },
+  {
+    "revision": "f31131e78bb90454f862ba6144061408",
+    "url": "/index.html"
+  }
+];
